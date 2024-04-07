8a0e2b1c1ba8629b61a92b9097c6ad8eb5690f74
Maximus Smith <maximusmith529@gmail.com>
2024-04-07 01:17:06 -0400

    works W

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 const port = 2364;
 
-
+app.use(express.static('public'));
 app.use(express.json());
 app.use(cors());
 app.set('view engine', 'ejs');
